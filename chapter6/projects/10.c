e526f88cff6abb88f37dcf01262b320c3c37d1f5
Bryan Márquez Albandoz <69647237+BryanMarqz@users.noreply.github.com>
2026-01-29 00:16:29 -0400

    Adding more solutions.

--- chapter6/projects/10.c
+++ chapter6/projects/10.c
@@ -1,16 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 int main(void)
 {
-	int min_m, min_d, min_y;
+	int min_m, min_d, min_y, new_m, new_d, new_y;
 	
+	printf("Enter a date (mm/dd/yyyy): ");
+	scanf("%d /%d /%d", &min_m, &min_d, &min_y);
+
+	if(min_m == 0 && min_d == 0 && min_y == 0)
+		exit(EXIT_SUCCESS);
+
+	if(min_m <= 0 || min_d <= 0 || min_y <= 0)
+	{
+		printf("Enter a valid date.\n");
+		exit(EXIT_SUCCESS);
+	}
+
 	do
 	{
 
 		printf("Enter a date (mm/dd/yyyy): ");
-		scanf("%d /%d /%d", &min_m, &min_d, &min_y);
+		scanf("%d /%d /%d", &new_m, &new_d, &new_y);
 
-	} while((min_m != 0) && (min_d != 0) && (min_y != 0));
+		if(new_m == 0 && new_d == 0 && new_y == 0)
+			break;
+
+		if(new_m <= 0 || new_d <= 0 || new_y <= 0)
+		{
+			printf("Enter a valid date.\n");
+			exit(EXIT_SUCCESS);
+		}
+
+		else if((new_y < min_y) ||
+			(new_y == min_y) && (new_m < min_m) ||
+			(new_y == min_y) && (new_m == min_m) && (new_d < min_d))	
+		{
+			min_m = new_m, min_d = new_d, min_y = new_y;
+		}
+
+	} while(true);
 	
 	printf("%d/%d/%d is the earliest date\n", min_m, min_d, min_y);
 
