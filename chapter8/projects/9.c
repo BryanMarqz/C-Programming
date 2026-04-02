bfd79d3f8564fb02f6d2e3961a9e933994294101
Bryan Márquez Albandoz <69647237+BryanMarqz@users.noreply.github.com>
2026-04-02 01:30:15 -0400

    9.c redo by me.

--- chapter8/projects/9.c
+++ chapter8/projects/9.c
@@ -1,79 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h> /* srand(), rand() */
-#include <time.h> /* time() */
+#include <stdlib.h>
+#include <time.h>
 
-int main(void) {
+#define N 10
 
-    char letter = 'A', 
-         grid[10][10] = {0};
-    int i = 0,
-        j = 0,
-        up = 0,   /* directions are essentially bools, written as ints to */
-        down = 0, /* conform with C89 standard.                           */
-        left = 0,
-        right = 0,
-        move = 0;
+int main(void)
+{
+	int row, col, direction[4] = {0, 1, 2, 3}, temp_row, temp_col;
+	char board[N][N], letters = 'A';
+	
+	for(row = 0; row < N; row++)
+	{
+		for(col = 0; col < N; col++)
+			board[row][col] = 0;
+	}
+	
+	board[0][0] = letter++;
+	
+	while(letter <= 'Z')
 
-    srand((unsigned) time(NULL));
-
-    grid[i][j] = letter++;
-
-    while (letter <= 'Z') {
-        up = down = left = right = move = 0;
-
-        if (j + 1 < 10 && grid[i][j + 1] == 0)
-            up = 1;
-        if (j - 1 >= 0 && grid[i][j - 1] == 0)
-            down = 1;
-        if (i + 1 < 10 && grid[i + 1][j] == 0)
-            right = 1;
-        if (i - 1 >= 0 && grid[i - 1][j] == 0)
-            left = 1;
-
-        if (up + down + left + right == 0)
-            break;
-
-        move = rand() % 4;
-        
-
-        /* Intentional fallthrough if direction fails */
-        switch(move) {
-            case 0:
-                if (up) {
-                    grid[i][++j] = letter++;
-                    break;
-                }
-            case 1:
-                if (down) {
-                    grid[i][--j] = letter++;
-                    break;
-                }
-            case 2:
-                if (right) {
-                    grid[++i][j] = letter++;
-                    break;
-                }
-            case 3:
-                if (left) {
-                    grid[--i][j] = letter++;
-                    break;
-                }
-            default:
-                break;
-        }
-    }
-
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            if (grid[i][j] == 0) 
-                grid[i][j] = '.';
-            printf("%c ", grid[i][j]);
-        }
-        printf("\n");
-    }
-    
-    return 0;
+	return 0;
 }
-
-    
-    
